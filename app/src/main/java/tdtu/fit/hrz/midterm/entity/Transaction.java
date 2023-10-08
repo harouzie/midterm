457876550a0b2074150ba6297b0603ddce08933b
harouzie <dophamquanghung@gmail.com>
2023-10-09 00:18:56 +0700

    [DEV] RecyclerView(RCV) Implementation - Transaction.java: complete properties, add setters & getters - TransactionCategory.java: category for Transaction - transactionlist_item.xml: row xml for RCView - TransactionListAdapter.java: data adapter for RCView

--- app/src/main/java/tdtu/fit/hrz/midterm/entity/Transaction.java
+++ app/src/main/java/tdtu/fit/hrz/midterm/entity/Transaction.java
@@ -1,5 +1,57 @@
 package tdtu.fit.hrz.midterm.entity;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Currency;
+import java.util.Date;
 
 public class Transaction {
-    private int transactionId;
+    private static int staticTransactionId = 0;
+    private final int transactionId;
+    private Currency currency;
+    private float spentAmount = 0.0f;
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("dd.mm.yyyy");
+    private Date spentDate;
+    private TransactionCategory category;
+
+    public Transaction() {
+        this.transactionId = staticTransactionId + 1;
+        currency = Currency.getInstance("VND");
+        spentDate = new Date();
+        category = TransactionCategory.GENERAL_FEE;
+        staticTransactionId +=1;
+    }
+
+
+    public float getSpentAmount() {
+        return spentAmount;
+    }
+    //================================================
+    public int getTransactionId() {
+        return transactionId;
+    }
+    //================================================
+    public Date getSpentDate() {
+        return spentDate;
+    }
+    public String getSpentDateString() {
+        return dateFormat.format(spentDate);
+    }
+
+    public void setSpentDate(Date spentDate) {
+        this.spentDate = spentDate;
+    }
+    //================================================
+    public TransactionCategory getCategory() {
+        return category;
+    }
+    public void setCategory(TransactionCategory category) {
+        this.category = category;
+    }
+    //================================================
+    public Currency getCurrency() {
+        return currency;
+    }
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
 }
