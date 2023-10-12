d504d3dd8cd8de9363c12a80afe67b559a097f0d
harouzie <dophamquanghung@gmail.com>
2023-10-12 21:17:05 +0700

    [DEV] Alltime history activity init

---
+++ app/src/main/java/tdtu/fit/hrz/midterm/AlltimeHistoryActivity.java
@@ -0,0 +1,14 @@
+package tdtu.fit.hrz.midterm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlltimeHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alltime_history);
+    }
+}
