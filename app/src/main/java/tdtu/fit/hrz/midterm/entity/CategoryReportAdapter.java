40775ae93136d114974977d3360999a1d0ed5711
harouzie <dophamquanghung@gmail.com>
2023-10-16 00:32:20 +0700

    [DEV] patch 1.9 CategoryReportAdapter -> to RCV finish functional CategoricalStatisticActivity DAO update get CategoryReport

--- app/src/main/java/tdtu/fit/hrz/midterm/entity/CategoryReportAdapter.java
+++ app/src/main/java/tdtu/fit/hrz/midterm/entity/CategoryReportAdapter.java
@@ -1,6 +1,8 @@
 package tdtu.fit.hrz.midterm.entity;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,68 +10,61 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.ArrayList;
 
 import tdtu.fit.hrz.midterm.R;
+import tdtu.fit.hrz.midterm.TransactionDetailActivity;
+import tdtu.fit.hrz.midterm.entity.CategoryReportHolder;
 
-public class CategoryReportAdapter extends BaseAdapter {
+public class CategoryReportAdapter extends RecyclerView.Adapter<CategoryReportHolder> {
 
     private final Context context;
-    private final ArrayList<CategoryReport> items;
+    private final ArrayList<CategoryReport> reports;
     private final int resourceId;
+    private LayoutInflater mInflater;
 
-    ImageView category_icon;
-    TextView  category_name,category_amount,
-            category_currency,category_percentage,category_ranking;
     public CategoryReportAdapter(Context context, ArrayList<CategoryReport> items, int resourceId) {
+        super();
         this.context = context;
-        this.items = items;
+        this.reports = items;
         this.resourceId = resourceId;
+        this.mInflater = LayoutInflater.from(context);
     }
 
+    /**
+     * @param parent   The ViewGroup into which the new View will be added after it is bound to
+     *                 an adapter position.
+     * @param viewType The view type of the new View.
+     * @return @CategoryReportHolder
+     */
+    @NonNull
     @Override
-    public int getCount() {
-        return items.size();
+    public CategoryReportHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View mItemView = mInflater.inflate(this.resourceId,parent, false);
+        return new CategoryReportHolder(mItemView,
+                this.context, this.resourceId);
     }
 
+    /**
+     * @param holder   The ViewHolder which should be updated to represent the contents of the
+     *                 item at the given position in the data set.
+     * @param position The position of the item within the adapter's data set.
+     */
     @Override
-    public Object getItem(int i) {
-        return items.get(i);
+    public void onBindViewHolder(@NonNull CategoryReportHolder holder, int position) {
+        CategoryReport report = reports.get(position);
+        holder.update(report);
     }
 
+    /**
+     * @return
+     */
     @Override
-    public long getItemId(int i) {
-        return 0;
+    public int getItemCount() {
+        return reports.size();
     }
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        if(convertView == null){
-            convertView = LayoutInflater
-                    .from(context)
-                    .inflate(this.resourceId, parent, false);
-        }
-        CategoryReport report = items.get(position);
-        category_icon = convertView.findViewById(R.id.category_icon);
-        category_name = convertView.findViewById(R.id.category_name);
-        category_amount = convertView.findViewById(R.id.category_amount);
-        category_currency = convertView.findViewById(R.id.category_currency);
-        category_percentage = convertView.findViewById(R.id.category_percentage);
-        category_ranking = convertView.findViewById(R.id.category_ranking);
-
-        category_icon.setImageResource(report.getCategory().getResourceId());
-        category_name.setText(String.format("%s", report.getCategory()));
-        category_amount.setText(
-                MyStringFormatter.numberFormat.format(report.getTotalSpent()));
-        category_currency.setText(
-                String.format("%s", report.getCurrency().getCurrencyCode()));
-
-        if (TransactionCategory.INCOME_GIFT.equals(report.getCategory())
-                || TransactionCategory.INCOME_SALARY.equals(report.getCategory())){
-            category_amount.setTextColor(context.getResources().getColor(R.color.green));
-        } else category_amount.setTextColor(context.getResources().getColor(R.color.dark_red));
-
-
-        return convertView;
-    }
 }
